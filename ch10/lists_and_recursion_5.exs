f000398c8a5ab74c031f5b2d677d9262f73ff7be
Nick Cox <nick@nickcox.me>
2017-09-25 21:56:23 -0700

    Re-implement `all?` without Enum module

--- ch10/lists_and_recursion_5.exs
+++ ch10/lists_and_recursion_5.exs
@@ -1,9 +1,26 @@
 defmodule MyList do
-  def all?(list, thing) do
-    num_things = list
-    |> Enum.filter(&(&1 == thing))
-    |> length
-    num_things == length(list)
+  # def all?(list, thing) do
+  #   num_things = list
+  #   |> Enum.filter(&(&1 == thing))
+  #   |> length
+  #   num_things == length(list)
+  # end
+
+  def all?(list, func) do
+    result = _all(list, func, [])
+    length(result) == length(list)
+  end
+
+  defp _all(list, func, result) when length(list) > 0 do
+    [head | tail] = list
+    if func.(head) do
+      _all(tail, func, [head | result])
+    else
+      _all(tail, func, result)
+    end
+  end
+  defp _all(_, _, result) do
+    result
   end
 
   def each(list, func) do
@@ -25,7 +42,7 @@ defmodule MyList do
     end
   end
 
-  defp _filter(list, _, result) do
+  defp _filter(_, _, result) do
     result
   end
 end
